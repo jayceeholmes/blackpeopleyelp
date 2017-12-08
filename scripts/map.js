2bd56aad23ef595e514afda3e2a91938579b13b6
jayceeholmes <jhh457@nyu.edu>
2017-12-08 08:21:49 -0500

    Map Fixes 1

--- scripts/map.js
+++ scripts/map.js
@@ -2,13 +2,41 @@
 
 // SHOW MAP
 // -----------------------------------------------------------------------------
-    mapboxgl.accessToken = 'pk.eyJ1IjoiamF5Y2VlaG9sbWVzIiwiYSI6ImNqOXVkM2d3dTAxYTAycWwwejUydnhxcTAifQ.B-xBaoNaMrQug0ibwBBP-w';
-    var map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/jayceeholmes/cj9vhztp44udf2sp7i6yjr91z',
-      zoom: 13,
-      center: [4.899, 52.372]
-    });
-
-// SHOW OVERLAY
+    var key = 'pk.eyJ1IjoiamF5Y2VlaG9sbWVzIiwiYSI6ImNqOXVkM2d3dTAxYTAycWwwejUydnhxcTAifQ.B-xBaoNaMrQug0ibwBBP-w';
+ // MAPPA SCRIPTS TO SHOW OVERLAY
 // -----------------------------------------------------------------------------
+
+// Create a variable to hold our map
+var myMap;
+// Create a variable to hold our canvas
+var canvas;
+// Create a new Mappa instance using Leaflet.
+var mappa = new Mappa('Mapboxgl', key);
+
+// Lets put all our map options in a single object
+var options = {
+  lat: 40.730042,
+  lng: -73.993964,
+  zoom: 12,
+  style: 'mapbox://styles/jayceeholmes/cj9vhztp44udf2sp7i6yjr91z',
+}
+
+
+// p5.js setup
+function setup(){
+  // Create a canvas 640x640
+  canvas = createCanvas(window.innerWidth, window.innerHeight);
+
+  // Create a tile map with lat 0, lng 0, zoom 4
+  myMap = mappa.tileMap(options);
+
+  // Overlay the tilemap on top of the canvas
+  myMap.overlay(canvas);
+
+  
+
+}
+
+// p5.js draw
+function draw(){
+}
